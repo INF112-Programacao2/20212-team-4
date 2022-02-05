41ff4b7cc171bc45f6a4870bd0d459651e81282f
Maria Victória Fernandes Vaz <94924671+mvictoriaufv25@users.noreply.github.com>
2022-02-05 20:44:17 -0300

    mudando nome do personagem

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 
 /* PERSONAGENS  */
 Protagonista Player(10, 3);
-Inimigo Johnny("Ambrosio", 12, 3, 2);
+Inimigo Johnny("Johnny", 12, 3, 2);
 Inimigo Xerife_Espeto("Xerife Espeto", 24, 4, 4);
 Inimigo Jose_do_Caixao("Jose do caixao", 32, 4, 3);
 Inimigo Caixao_do_Jose("Caixao do Jose", 15, 2, 1);
