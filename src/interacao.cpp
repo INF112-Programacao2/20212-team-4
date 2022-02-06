26bd2cabc03ccf76fc9fe9f6d9871909930cb2eb
Colorida <92737180+laracolorida@users.noreply.github.com>
2022-02-06 17:44:11 -0300

    inicio da implementação dos itens coletaveis

---
+++ src/interacao.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include "interacao.hpp"
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+
+Interacao::Interacao(std::string nome, short int X, short int Y){
+    _nome = nome;
+    _x = X;
+    _y = Y;
+    }
+
+std::string Interacao::getNome(){
+    return _nome;
+}
+
+short int Interacao::getPosicaoX(){
+    return _x;
+}
+
+short int Interacao::getPosicaoY(){
+    return _y;
+}
+
+char Interacao::setValor(char valor){
+    _valor = valor;
+}
+char Interacao::getValor(){
+    return _valor;
+}
+
+
+
+void Item::desenhaItem(){
+    MAPA[_x][_y] = al_draw_bitmap(_imgItem);
+}
+
+void Item::Item(ALLEGRO_BITMAP img){
+    _imgItem = al_load_bitmap(img);
+}
+
+void Item::removeItem(){
+    MAPA[_x][_y] = 1;
+}
---
+++ src/interacao.hpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+
+class Interacao {
+    private:
+    std::string _nome;
+    short int _x, _y;
+    char _valor;
+
+    public:
+    Interacao(std::string nome, short int X, short int Y);
+    std::string getNome();
+    short int getPosicaoX();
+    short int getPosicaoY();
+    char getValor();
+    char setValor(char valor);
+}
+
+
+
+class Item: public Interacao{
+    private:
+    ALLEGRO_BITMAP = _imgItem;
+    public:
+    Item();
+    void desenhaItem();
+    void removeItem();
+}
